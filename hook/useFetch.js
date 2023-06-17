c4578f2900080903ef279ace76e77d12256e5a2f
Ʀɨ¢Ϧ♬Ʀᖱន០⩎ <99025830+Daniel-V-Richardson@users.noreply.github.com>
2023-06-17 08:18:49 +0530

    Update useFetch.js

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -10,7 +10,7 @@ const useFetch = (endpoint, query) => {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": "398bfaec23mshe97ee64121ba26ap1e914ejsn4d641ec4cbab",
+      "X-RapidAPI-Key": "",
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: { ...query },
